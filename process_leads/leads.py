44638405dabe67ddee94e05b3f2abe63081c4a88
sajay <sajaykannan10@gmail.com>
2026-01-08 15:05:04 +0530

    changed rows cols

--- process_leads/leads.py
+++ process_leads/leads.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 leads = np.array([
-    [12, 18, 10, 15, 5],   # Sunday
-    [20, 25, 22, 18, 8],   # Monday
-    [18, 30, 25, 22, 10],  # Tuesday
-    [25, 28, 30, 26, 12],  # Wednesday
-    [30, 35, 28, 32, 15],  # Thursday
-    [40, 45, 38, 40, 20],  # Friday
-    [35, 50, 42, 45, 25]   # Saturday
+    # s  m    t  w   th
+    [12, 18, 10, 15, 5],   # facebook
+    [20, 25, 22, 18, 8],   # insta
+    [18, 30, 25, 22, 10],  # youtube
+    [25, 28, 30, 26, 12],  # refferal
+    [30, 35, 28, 32, 15],  # walkin
+    [40, 45, 38, 40, 20],  # reddit
+    [35, 50, 42, 45, 25]   # camp
+    
 ])
 
