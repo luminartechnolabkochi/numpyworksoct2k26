e338caf765775bfe94fe2a717241bb58952ad936
sajay <sajaykannan10@gmail.com>
2026-01-13 13:48:27 +0530

    tes

--- fileworks/titanic_case_study/process_titanic_dat.py
+++ fileworks/titanic_case_study/process_titanic_dat.py
@@ -3,5 +3,10 @@ import numpy as np
 
 data = np.genfromtxt("fileworks\\titanic_case_study\\passenger_data.csv",delimiter=",",skip_header=1,filling_values=0,dtype="str")
 
-print(data.shape)
+# total number of passengers
+print(f"total number of passengers {data.shape}")
 
+# survival analysis
+
+# total number of survivals
+# total numvber of death
